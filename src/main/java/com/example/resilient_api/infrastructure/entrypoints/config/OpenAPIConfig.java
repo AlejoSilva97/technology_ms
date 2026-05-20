406499f5ffcfc6a832d4b1af92361a0b56c55bb3
AlejoSilva97 <alejandrosilva1897@gmail.com>
2026-05-20 14:52:18 -0500

    feat: implementacion de swagger en technology_ms

---
+++ src/main/java/com/example/resilient_api/infrastructure/entrypoints/config/OpenAPIConfig.java
@@ -0,0 +1,19 @@
+package com.example.resilient_api.infrastructure.entrypoints.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Resilient Technology API")
+                        .version("1.0.0")
+                        .description("Microservicio reactivo para la gestión de tecnologías utilizando Arquitectura Hexagonal."));
+    }
+}
